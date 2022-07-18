109270b1d0c7feb6c53c1c7aaf6129be33b188c6
tanmaygautam1 <70182169+tanmaygautam1@users.noreply.github.com>
2022-07-18 14:37:07 +0530

    Update Stack.cpp

--- Stacks/Stack.cpp
+++ Stacks/Stack.cpp
@@ -1,4 +1,57 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define max 1000
+
+class Stack{
+  int top;
+  public:
+  int a[max];
+  Stack(){top=-1;}
+  bool push(int x);
+  int pop();
+  int peek();
+  bool isEmpty();
+
+};
+bool Stack::push(int x){
+  if(top>=(max-1)){
+    cout<<"Stack overflow";
+    return false;}
+  else {
+    a[++top]=x;
+    cout<<x<<" pushed into the stack\n";
+    return true;
+  }
+  }
+int Stack::pop(){
+  if(top<0){
+    cout<<"Stack Underflow";
+    return 0;}
+    else{
+      int x= a[top--];
+      return x;
+    }
+}  
+int Stack::peek(){
+  if(top<0){
+    cout<<"Stack empty";
+    return 0;
+  }
+  else{
+    int x=a[top];
+    return x;
+  }
+}
+bool Stack::isEmpty()
+{
+    return (top < 0);
+}
 int main(){
-return 0; }
+  class Stack s;
+  s.push(10);
+  s.push(10);
+  s.push(500);
+  s.push(500);
+  s.pop();
+  cout<<s.peek();
+  }
