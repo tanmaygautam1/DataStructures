c56bf239d5cf88c8377ed1f38d2423d36442052a
tanmaygautam1 <70182169+tanmaygautam1@users.noreply.github.com>
2022-08-01 21:13:15 +0530

    Create EqualSumPartition.cpp

---
+++ DP/EqualSumPartition.cpp
@@ -0,0 +1,29 @@
+#include <bits/std/c++.h>
+using namespace std;
+
+bool(int arr[], int n){
+  int sum=0;
+for(int i=0;i<n;i++){
+sum=sum+a[i];
+}
+  Sum=sum/2;
+if(sum%2!=0)
+  return false;
+  
+else{
+  int A[n+1][Sum+1]
+for(int i=0; i<n+1;i++){
+  for(int j=0;j<Sum+1;j++){
+  if(i==0){
+  A[i][j]=false;}
+  if(j==0){
+  A[i][j]=true;}}
+  for(int i=1; i<n+1;i++){
+  for(int j=1;j<Sum+1;j++){
+  if(arr[i-1]<=Sum){
+  A[i][j]=A[i-1][Sum-A[i-1]] || }
+  }}
+  
+}
+  
+  }
