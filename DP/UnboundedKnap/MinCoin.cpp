6433c318f62916e9d7d926b4171a6ffe6ff8a8c3
tanmaygautam1 <70182169+tanmaygautam1@users.noreply.github.com>
2022-08-02 22:50:43 +0530

    Create MinCoin.cpp

---
+++ DP/UnboundedKnap/MinCoin.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+MinCoin(int coins[], int n, int sum){
+int A[n+1][sum+1]
+  for(int i=0; i<n+1;i++){
+  for (int j=0; j<sum+1;j++){
+  if(j==0){A[i][j]=0;}
+    if(i==0){A[i][j]=INT_MAX-1;}
+  }}
+  for(int i=1;j<Sum+1;j++){
+  if(j%coins[i-1]==0)
+    t[i][j]=j/arr[0];
+  else
+    t[i][j]=INT_MAX-1;}
+  }
