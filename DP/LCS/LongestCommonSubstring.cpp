e3727dffffd27202b447fecc7f6e18c362b701bb
tanmaygautam1 <70182169+tanmaygautam1@users.noreply.github.com>
2022-08-08 19:00:25 +0530

    Update LongestCommonSubstring.cpp

--- DP/LCS/LongestCommonSubstring.cpp
+++ DP/LCS/LongestCommonSubstring.cpp
@@ -1,18 +1,4 @@
-#include <bits/std++.h>
-using namespace std;
-
-int Count(int x[], int y[], int m, int n){
-int A[m+1][n+1];
-for(int i=0; i<m+1;i++){
-for(int j=0;j<n+1;j++){
-if(i==0 && j==0){
-A[i][j]=0;
-}}}
-  for(int i=1;i<m+1;i++){
-    for(int j=1;j<n+1;j++){
-        if(x[i-1]==y[j-1]){
-            A[i][j]= 1+A[i-1][j-1];}
-        else#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int Count(string x,string y, int m, int n){
@@ -40,24 +26,11 @@ int max=0;
 return max;
 }
 
-int main(){
-string x= "abcef";
-string y="abcdf";
-int m=5;
-  int n=5;
-  int a=Count(x,y,m,n);
-  cout<<a;
-}
-        A[i][j]= 0;
-        
-    }}
 
-return A[m][n];
-}
 
 int main(){
-string x= "abcefg";
-string y="abcdfg";
+string x= "abdefg";
+string y="abedfg";
 int m=6;
   int n=6;
   int a=Count(x,y,m,n);
